fa32a7cf01d24d9ba718160319201c3e17b4e2d6
mohammad <mfelfelani72@gmail.com>
2025-03-26 12:27:25 +0000

    first edit

--- src/app/utils/constant/Routes.js
+++ src/app/utils/constant/Routes.js
@@ -6,36 +6,7 @@ export const ALL_ROUTES = [
   // Auth
   "/",
   "/landing",
-  "/dashboard",
-  "/login",
-  "/sign-up",
-  "/confirm",
-  "/change-password",
-  "/change-password-confirm",
-  // Markets
-  "/market",
-  // Trades
-  "/trades",
-  // Wallet
-  "/wallet",
-  "/wallet/deposit",
-  "/wallet/withdraw",
-  // Credit
-  "/credit",
-  // Profile
-  "/profile",
-  "/profile/invite-friends",
-  "/profile/edit-profile",
-  "/profile/change-avatar",
-  "/profile/authentication",
-  "/profile/user-level-up",
-  // CardManagement
-  "/card-management",
-  "/card-management/add-card",
-  // Notification
-  "/notification",
-  // Camera
-  "/camera",
+  
 ];
 
 export const NOT_RELOAD_ROUTES = [
