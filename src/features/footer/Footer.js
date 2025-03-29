920a3a28273e1d61a295a6e7aa851d01c637e8fe
mohammad <mfelfelani72@gmail.com>
2025-03-29 10:41:00 +0000

    edit

--- src/features/footer/Footer.js
+++ src/features/footer/Footer.js
@@ -1,9 +1,19 @@
 import React from "react";
 
+import vector from "../../../assets/images/vector/vector-footer.svg";
+import { ImageLazy } from "../core/components/Image.jsx";
+
 const Footer = () => {
   return (
     <>
-      <div className="flex flex-col bg-lime-400">Footer</div>
+      <div className="flex flex-col bg-lime-500">
+        <div className="relative">
+          <div className="absolute top-0 z-30 w-full">
+            <ImageLazy src={vector} alt="vector-footer" />
+          </div>
+          <div className="absolute top-1 inset-x-0 w-full h-[730px] bg-[#28263A]"></div>
+        </div>
+      </div>
     </>
   );
 };
