0aea720b2884e8dba85a02ce082610c5944e4ca1
Mohammad <mfelfelani72@gmail.com>
2025-04-08 07:38:39 +0000

    edit

--- src/features/home/components/customerReview/ReviewBox.jsx
+++ src/features/home/components/customerReview/ReviewBox.jsx
@@ -15,12 +15,24 @@ import customer from "../../../../../assets/images/png/customer.png"
 const ReviewBox = ({ className, ...props }) => {
     return (
         <>
-            <div className={cn("flex flex-col items-center w-96 h-72 bg-white rounded-[20px] shadow-[0px_42.444149017333984px_61.30821228027344px_0px_rgba(0,0,0,0.08)] pt-11", className)} >
-                <ImageLazy src={customer} alt="customer" className={"w-11 h-11 rounded-full"} />
-                <div className="text-base font-bold leading-7 mt-2">سعید محمدی</div>
-                <div className="text-Neutral-300 text-sm font-normal leading-snug">تحلیل‌گر بازارهای مالی</div>
-                <div className="pt-4 px-6 text-center text-Neutral-400 text-base font-normal leading-7">ایمون هاب بهم کمک کرد که سریع‌تر از رقبا به اخبار مهم دسترسی داشته باشم. حالا می‌تونم تحلیل‌هام رو با اطلاعات دقیق‌تری انجام بدم و تصمیمات بهتری بگیرم</div>
+            <div className="relative">
+                {/* color shadow */}
+                {props?.colorShadow && <div className="absolute -left-[1.875rem] top-[3rem] -z-10 w-[25.5rem] h-[18.75rem] blur-2xl rounded-[3.75rem] opacity-40 review-box-shadow-gradiant"></div>}
+
+                {/* Review box */}
+                <div className={cn(
+                    "flex flex-col items-center w-96 h-72 bg-white rounded-[20px] pt-11 relative",
+                    className
+                )}>
+                    <ImageLazy src={customer} alt="customer" className="w-11 h-11 rounded-full" />
+                    <div className="text-base font-bold leading-7 mt-2">{props?.name}</div>
+                    <div className="text-Neutral-300 text-sm font-normal leading-snug">{props?.bio}</div>
+                    <div className="pt-4 px-6 text-center text-Neutral-400 text-base font-normal leading-7">
+                        {props?.review}
+                    </div>
+                </div>
             </div>
+
         </>
     )
 }
