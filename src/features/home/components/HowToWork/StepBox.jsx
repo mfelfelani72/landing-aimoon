880bf7b1495200a2abb024606b4803bc3f449c8f
Mohammad <mfelfelani72@gmail.com>
2025-04-09 12:02:52 +0000

    edit

---
+++ src/features/home/components/HowToWork/StepBox.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const StepBox = () => {
+    return (
+        <>
+            <div className='flex flex-row'>
+                <div className="text-secondary-100 text-9xl font-normal font-spaceGrotesk">1</div>
+                <div className="flex flex-col gap-3 px-[4rem]">
+                    <div className="text-primary-400 text-2xl font-medium text-right">جمع آوری لحظه ای خبر</div>
+                    <div className="w-60 text-Neutral-500 text-base font-medium">بات به‌صورت خودکار از بیش لز 1000  منابع خبری معتبر داده‌ها رو دریافت می‌کنه.(مثلاً سایت‌های خبری کریپتو، توییتر، ردیت و ...)</div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default StepBox
--- src/features/home/containers/HowToWork.js
+++ src/features/home/containers/HowToWork.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { useTranslation } from "react-i18next";
 
+// Components
+
+import StepBox from '../components/HowToWork/StepBox.jsx';
+
 const HowToUse = () => {
     // hooks
     const { t } = useTranslation();
     return (
         <>
+            {/* top section -> { top table and title } */}
             <div className='relative w-full h-[26.5rem] overflow-hidden -bottom-[19rem] -mt-[35rem] z-10'>
                 {/* table top*/}
 
@@ -20,6 +25,7 @@ const HowToUse = () => {
                         </div>
                     </div>
                 ))}
+
                 <div className={`absolute -left-[34rem] -top-[25rem] mx-[calc(50%-50rem)]`}>
                     <div className='flex flex-col gap-[86px] w-full items-center'>
                         <div className="w-[2000px] h-[2px] origin-top-left rotate-[25deg] opacity-5 bg-black border border-black" />
@@ -32,9 +38,6 @@ const HowToUse = () => {
                     </div>
                 </div>
 
-
-                {/* table top*/}
-
                 {/* title */}
                 <div className='absolute top-[20rem] inset-x-0 w-full inline-flex items-center justify-center '>
                     <div className="text-center text-Neutral-500 text-3xl font-bold">
@@ -42,11 +45,30 @@ const HowToUse = () => {
                     </div>
                 </div>
             </div>
-            <div className='relative w-full h-[50rem] overflow-hidden -bottom-[19rem]'>
 
-                {/* table bottom*/}
+            {/* bottom section -> { content and bottom table } */}
+            <div className='relative w-full h-[73rem] overflow-hidden -bottom-[19rem]'>
 
-                <div className='absolute top-[4rem] right-0 mx-[calc(50%-50rem)]'>
+                {/* content */}
+
+                <div className='absolute top-0  mx-[calc(50%-50rem)] width-theme'>
+
+                    <div className='flex flex-row'>
+                        <div className='basis-5/12 flex flex-col gap-[262px] pr-[22rem]'>
+                            <StepBox />
+                            <StepBox />
+                        </div>
+                        <div className='w-[8rem] inline-flex justify-start bg-lime-200'>sad</div>
+                        <div className='basis-5/12 flex flex-col mt-[15rem] gap-[262px]'>
+                            <StepBox />
+                            <StepBox />
+                        </div>
+                    </div>
+                </div>
+
+                {/* bottom table */}
+
+                <div className='absolute top-[27rem] right-0 mx-[calc(50%-50rem)]'>
                     <div className='flex flex-col gap-[86px]'>
                         <div className="w-[1700px] h-[2px] origin-top-right -rotate-[25deg] opacity-5 bg-black border border-black" />
                         <div className="w-[1700px] h-[2px] origin-top-right -rotate-[25deg] opacity-5 bg-black border border-black" />
@@ -69,7 +91,6 @@ const HowToUse = () => {
                         </div>
                     </div>
                 ))}
-                {/* table bottom*/}
 
             </div>
         </>
