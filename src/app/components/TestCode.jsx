196d4031156c58859303efdacbfd8075309877cd
Mohammad <mfelfelani72@gmail.com>
2025-04-12 08:29:00 +0000

    edit

--- src/app/components/TestCode.jsx
+++ src/app/components/TestCode.jsx
@@ -8,7 +8,7 @@ const TestCode = () => {
     <>
 
 
-      <ChessBoard className={"rotate-[45deg] skew-x-[-45deg]"} bordClassName={`grid-cols-12 grid-rows-12 w-[100rem] h-[100rem]`} backgroundColor={"bg-[#f2f2f3]"} mosaicClassName={"border-Neutral-100/50"} />
+      <ChessBoard className={"rotate-[25deg] skew-x-[-25deg]"} bordClassName={`grid-cols-12 grid-rows-12 w-[100rem] h-[100rem]`} backgroundColor={"bg-[#f2f2f3]"} mosaicClassName={"border-Neutral-100/50"} />
 
     </>
   );
