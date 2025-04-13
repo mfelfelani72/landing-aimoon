0b873ffe923e365c342ef35983582fee8f7d4c61
Mohammad <mfelfelani72@gmail.com>
2025-04-13 08:13:52 +0000

    edit

--- src/features/home/containers/WhyAimoonhub.js
+++ src/features/home/containers/WhyAimoonhub.js
@@ -11,13 +11,13 @@ import vector from "../../../../assets/images/vector/vector-why-aimoonhub.svg"
 const WhyAimoonhub = () => {
     return (
         <>
-            <div className="flex flex-col items-center justify-center w-full h-full overflow-x-hidden pointer-events-none">
-                <div className="relative w-full h-[82.5rem] z-20">
-                    <div className='absolute top-0 left-[56.25rem]'>
-                        <Image src={vector} alt="plans-vevtor" className={"object-none"} />
-                    </div>
+            <div className="flex flex-col items-center justify-center w-full h-full overflow-x-hidden">
+                <div className="relative w-[135rem] h-[82.5rem] z-20 ml-[20rem]">
+                    <Image src={vector} alt="plans-vevtor" className={"w-[135rem] h-full object-none"} />
+
                 </div>
             </div>
+
 
         </>
     )
