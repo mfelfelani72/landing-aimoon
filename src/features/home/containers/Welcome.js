e90f3afa4502d15737989a6ea1db71d824e1b193
Mohammad <mfelfelani72@gmail.com>
2025-04-14 07:04:45 +0000

    edit

--- src/features/home/containers/Welcome.js
+++ src/features/home/containers/Welcome.js
@@ -11,8 +11,12 @@ import { Image, ImageLazy } from '../../core/components/Image.jsx'
 
 import stars from "../../../../assets/icons/svg/stars.svg"
 import chart from "../../../../assets/images/svg/welcome-chart.svg"
-import shadow from "../../../../assets/images/svg/shadow-mockup.svg"
+import shadow from "../../../../assets/images/svg/shadow.svg"
 import mockup from "../../../../assets/images/png/mockup-aimoonhub.png"
+import coin_1 from "../../../../assets/icons/svg/icon-coin-1.svg"
+import usdt from "../../../../assets/icons/svg/icon-usdt.svg"
+import bitcoin from "../../../../assets/icons/svg/icon-bitcoin.svg"
+import eth from "../../../../assets/icons/svg/icon-eth.svg"
 
 const Welcome = () => {
     return (
@@ -36,23 +40,38 @@ const Welcome = () => {
                     <h4 className="w-96 text-zinc-700 text-base font-normal leading-relaxed mt-3">با دقتی فراتر از انسان، جدیدترین اخبار را بررسی کن. داده‌های لحظه‌ای، تحلیل‌های دقیق و بینشی عمیق برای تصمیم‌گیری بهتر در بازار.</h4>
 
                 </div>
-                <div className='relative w-[50rem] z-20 mt-[2rem]  pointer-events-none'>
+                {/* pictures */}
+                <div className='relative w-[50rem] z-20 mt-[4rem]  pointer-events-none'>
+                    {/* coins-icon */}
+                    <div className="absolute -top-[1rem] -right-[6rem]">
+                        <ImageLazy src={coin_1} alt={"coin_1"} className={"w-8 h-8"} />
+                    </div>
+                    <div className="absolute top-[20rem] -right-[8rem]">
+                        <ImageLazy src={eth} alt={"eth"} className={"w-8 h-8"} />
+                    </div>
+                    <div className="absolute top-[9rem] left-[15.5rem]">
+                        <ImageLazy src={usdt} alt={"usdt"} className={"w-10 h-10"} />
+                    </div>
+                    <div className="absolute top-[34rem] left-[15rem]">
+                        <ImageLazy src={bitcoin} alt={"bitcoin"} className={"w-8 h-8"} />
+                    </div>
+
                     <div className='absolute top-0 inset-x-0 w-52 h-[27rem] bg-white rounded-3xl shadow-[100px_50px_200px_0px_rgba(10,10,15,0.10)] inline-flex items-center'>
                         <ImageLazy src={mockup} alt="mockup" className={"w-52 h-96"} />
                     </div>
-                    <div className='absolute -top-[15rem] -right-[4rem] -z-1 opacity-60'>
-                        <ImageLazy src={shadow} alt="shadow" className={"w-[380px] h-[930px]"} />
+                    <div className='absolute -top-[1rem] -right-[1.25rem] -z-1'>
+                        <ImageLazy src={shadow} alt="shadow" className={"w-[260px] h-[480px] rounded-3xl blur-xl"} />
                     </div>
                     <div className='absolute top-[17.5rem] right-[3.5rem]'>
                         <ImageLazy src={chart} alt="welcome-chart" className={"w-[411px] h-[241px]"} />
                     </div>
                 </div>
 
-                <div className='absolute top-[4rem] right-[65rem]'>
+                <div className='absolute top-[6rem] right-[65rem]'>
                     <ChessBoard id={"table-work-bottom"} className={"rotate-[32deg] skew-x-[-32deg] skew-y-[-5deg]"} bordClassName={`grid-cols-16 grid-rows-16 w-[100rem] h-[100rem]`} backgroundColor={"bg-[#dadada]"} mosaicClassName={"border-[#dadada]"} />
                 </div>
 
-                <div className="absolute bottom-0 inset-x-0 w-full h-40 bg-gradient-to-t from-white to-transparent pointer-events-none z-20"></div>
+                <div className="absolute bottom-0 inset-x-0 w-full h-20 bg-gradient-to-t from-white to-transparent pointer-events-none z-20"></div>
             </div>
         </>
     )
