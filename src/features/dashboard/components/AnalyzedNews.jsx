04d909cf1513d0500c8bf9f2de057c400024a6b3
Mohammad <mfelfelani72@gmail.com>
2025-04-20 09:36:02 +0000

    edit

--- src/features/dashboard/components/AnalyzedNews.jsx
+++ src/features/dashboard/components/AnalyzedNews.jsx
@@ -7,8 +7,11 @@ import NewsBox from '../../core/components/NewsBox.jsx'
 const AnalyzedNews = () => {
     return (
         <>
-        <div className='mt-8'>
-            <NewsBox className={""} ></NewsBox>
+            <div className='mt-8 bg-Neutral-500 overflow-ellipsis'>
+                <NewsBox className={""} ></NewsBox>
+                <NewsBox className={""} ></NewsBox>
+                <NewsBox className={""} ></NewsBox>
+                <NewsBox className={""} ></NewsBox>
             </div>
         </>
     )
--- src/features/dashboard/containers/DashbaradHome.js
+++ src/features/dashboard/containers/DashbaradHome.js
@@ -29,73 +29,75 @@ const DashbaradHome = () => {
 
     return (
         <>
-            {/* login & register */}
+            <div className='flex flex-col h-full overflow-y-auto overflow-x-clip bg-Neutral-500'>
+                {/* login & register */}
 
-            <div className='flex flex-row gap-2 mx-6 my-8'>
-                <ButtonLink className={"w-44 h-10"}>{t("register")}</ButtonLink>
-                <ButtonLink className={"w-44 h-10 bg-[#28263A] hover:bg-gray-800"}>{t("login")}</ButtonLink>
+                <div className='flex flex-row gap-2 mx-6 my-8'>
+                    <ButtonLink className={"w-44 h-10"}>{t("register")}</ButtonLink>
+                    <ButtonLink className={"w-44 h-10 bg-[#28263A] hover:bg-gray-800"}>{t("login")}</ButtonLink>
+                </div>
+
+                {/* seprator */}
+
+                <div className="self-stretch h-0 opacity-20 outline-[0.61px] outline-offset-[-0.31px] outline-Neutral-300" />
+
+                {/* shortcuts */}
+
+                <div className="px-6 pt-4 text-right justify-start text-Neutral-300 text-xs font-normal leading-tight">{t("swipe_right")}</div>
+
+                <div className='flex flex-row px-6 py-2 gap-5'>
+                    <Link className='flex flex-col gap-2 justify-center items-center'>
+                        <div className='w-14 h-14 bg-[#11101A] inline-flex justify-center items-center rounded-full'>
+                            <Image src={icon_coin_dashboard} alt="icon_coin_dashboard" className={"w-6 h-6"} />
+                        </div>
+                        <div className="w-10 text-center text-sm font-normal leading-tight">{t("coins_dashboard")}</div>
+                    </Link>
+                    <Link className='flex flex-col gap-2 justify-center items-center'>
+                        <div className='w-14 h-14 bg-[#11101A] inline-flex justify-center items-center rounded-full'>
+                            <Image src={icon_latest_news} alt="icon_latest_news" className={"w-6 h-6"} />
+                        </div>
+                        <div className="w-10 text-center text-sm font-normal leading-tight">{t("latest_news")}</div>
+                    </Link>
+                    <Link className='flex flex-col gap-2 justify-center items-center'>
+                        <div className='w-14 h-14 bg-[#11101A] inline-flex justify-center items-center rounded-full'>
+                            <Image src={icon_author_dashboard} alt="icon_author_dashboard" className={"w-6 h-6"} />
+                        </div>
+                        <div className="w-10 text-center text-sm font-normal leading-tight">{t("authors_dashboard")}</div>
+                    </Link>
+                    <Link className='flex flex-col gap-2 justify-center items-center'>
+                        <div className='w-14 h-14 bg-[#11101A] inline-flex justify-center items-center rounded-full'>
+                            <Image src={icon_provider_dashboard} alt="icon_provider_dashboard" className={"w-6 h-6"} />
+                        </div>
+                        <div className="w-10 text-center text-sm font-normal leading-tight">{t("providers_dashboard")}</div>
+                    </Link>
+                    <Link className='flex flex-col gap-2 justify-center items-center'>
+                        <div className='w-14 h-14 bg-[#11101A] inline-flex justify-center items-center rounded-full'>
+                            <Image src={icon_news_analysis} alt="icon_news_analysis" className={""} />
+                        </div>
+                        <div className="w-10 text-center text-sm font-normal leading-tight">{t("news_analysis")}</div>
+                    </Link>
+                </div>
+
+                {/* seprator */}
+
+                <div className="self-stretch h-0 opacity-20 mt-8 outline-[0.61px] outline-offset-[-0.31px] outline-Neutral-300" />
+
+                <TabDefault
+                    id={"tab-0"}
+                    className={"pt-4 px-6"}
+                    titles={["analyzed_news", "latest_news"]}
+                    classNameTitle=""
+                    idTitle=""
+                    statePage={statePage}
+                    setStatePage={setStatePage}
+                    contents={[
+                        <AnalyzedNews />,
+                        <div>
+                            2
+                        </div>,
+                    ]}
+                />
             </div>
-
-            {/* seprator */}
-
-            <div className="self-stretch h-0 opacity-20 outline-[0.61px] outline-offset-[-0.31px] outline-Neutral-300" />
-
-            {/* shortcuts */}
-
-            <div className="px-6 pt-4 text-right justify-start text-Neutral-300 text-xs font-normal leading-tight">{t("swipe_right")}</div>
-
-            <div className='flex flex-row px-6 py-2 gap-5'>
-                <Link className='flex flex-col gap-2 justify-center items-center'>
-                    <div className='w-14 h-14 bg-[#11101A] inline-flex justify-center items-center rounded-full'>
-                        <Image src={icon_coin_dashboard} alt="icon_coin_dashboard" className={"w-6 h-6"} />
-                    </div>
-                    <div className="w-10 text-center text-sm font-normal leading-tight">{t("coins_dashboard")}</div>
-                </Link>
-                <Link className='flex flex-col gap-2 justify-center items-center'>
-                    <div className='w-14 h-14 bg-[#11101A] inline-flex justify-center items-center rounded-full'>
-                        <Image src={icon_latest_news} alt="icon_latest_news" className={"w-6 h-6"} />
-                    </div>
-                    <div className="w-10 text-center text-sm font-normal leading-tight">{t("latest_news")}</div>
-                </Link>
-                <Link className='flex flex-col gap-2 justify-center items-center'>
-                    <div className='w-14 h-14 bg-[#11101A] inline-flex justify-center items-center rounded-full'>
-                        <Image src={icon_author_dashboard} alt="icon_author_dashboard" className={"w-6 h-6"} />
-                    </div>
-                    <div className="w-10 text-center text-sm font-normal leading-tight">{t("authors_dashboard")}</div>
-                </Link>
-                <Link className='flex flex-col gap-2 justify-center items-center'>
-                    <div className='w-14 h-14 bg-[#11101A] inline-flex justify-center items-center rounded-full'>
-                        <Image src={icon_provider_dashboard} alt="icon_provider_dashboard" className={"w-6 h-6"} />
-                    </div>
-                    <div className="w-10 text-center text-sm font-normal leading-tight">{t("providers_dashboard")}</div>
-                </Link>
-                <Link className='flex flex-col gap-2 justify-center items-center'>
-                    <div className='w-14 h-14 bg-[#11101A] inline-flex justify-center items-center rounded-full'>
-                        <Image src={icon_news_analysis} alt="icon_news_analysis" className={""} />
-                    </div>
-                    <div className="w-10 text-center text-sm font-normal leading-tight">{t("news_analysis")}</div>
-                </Link>
-            </div>
-
-            {/* seprator */}
-
-            <div className="self-stretch h-0 opacity-20 mt-8 outline-[0.61px] outline-offset-[-0.31px] outline-Neutral-300" />
-
-            <TabDefault
-                id={"tab-0"}
-                className={"pt-4 px-6"}
-                titles={["analyzed_news", "latest_news"]}
-                classNameTitle=""
-                idTitle=""
-                statePage={statePage}
-                setStatePage={setStatePage}
-                contents={[
-                    <AnalyzedNews />,
-                    <div>
-                        2
-                    </div>,
-                ]}
-            />
         </>
     )
 }
