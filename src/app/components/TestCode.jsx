7f08bfc32999935e2b5de475f67bd8fb877722ca
Mohammad <mfelfelani72@gmail.com>
2025-04-29 10:52:08 +0000

    edit

--- src/app/components/TestCode.jsx
+++ src/app/components/TestCode.jsx
@@ -1,32 +1,56 @@
-import React, { useState } from 'react';
-import { LineChart } from '../../features/core/components/Chart.jsx';
+import React from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import HighchartsMore from 'highcharts/highcharts-more';
+import VariablePie from 'highcharts/modules/variable-pie';
+
 
 
 const CleanSplineChart = () => {
-  const [data] = useState([
-    {
-      name: 'Tokyo',
-      data: [5.2, 5.7, 8.7, 13.9, 18.2, 21.4, 25.0, 22.8, 17.5, 12.1, 7.6],
-      color: '#3B82F6'
+  const chartOptions = {
+    chart: {
+      type: 'variablepie'
     },
-    {
-      name: 'Bergen',
-      data: [1.6, 3.3, 5.9, 10.5, 13.5, 14.5, 14.4, 11.5, 8.7, 4.7, 2.6],
-      color: '#10B981'
+    title: {
+      text: 'Countries compared by population density and total area, 2024'
     },
-    {
-      name: 'dsfsdf',
-      data: [1.6, 3.3, 5.9, 10.5, 13.5, 12, 15, 11.5, 8.7, 4.7, 2.6],
-      color: '#54dfd5'
-    }
-  ]);
-
-
+    tooltip: {
+      headerFormat: '',
+      pointFormat: '<span style="color:{point.color}">\u25CF</span> <b>{point.name}</b><br/>' +
+        'Area (square km): <b>{point.y}</b><br/>' +
+        'Population density (people per square km): <b>{point.z}</b><br/>'
+    },
+    series: [{
+      minPointSize: 10,
+      innerSize: '30%',
+      zMin: 0,
+      name: 'Countries',
+      data: [{
+        name: 'Spain',
+        y: 505992,
+        z: 95
+      }, {
+        name: 'France',
+        y: 551695,
+        z: 118
+      }, {
+        name: 'Poland',
+        y: 312679,
+        z: 131
+      },],
+      colors: [
+        '#4caefe',
+        '#3dc3e8',
+        '#2dd9db',
+      ]
+    }]
+  };
 
   return (
-    <div className="mt-10 w-[20rem] mx-[1rem]">
-      <LineChart data={data} />
-    </div>
+    <HighchartsReact
+      highcharts={Highcharts}
+      options={chartOptions}
+    />
   );
 };
 
