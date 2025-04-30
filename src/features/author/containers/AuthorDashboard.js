85d3580f49912381298ec804d7c0f60e3577d1b8
pouyaWork <pouyasoltani81@gmail.com>
2025-04-30 12:09:42 +0330

    author sentiment

--- src/features/author/containers/AuthorDashboard.js
+++ src/features/author/containers/AuthorDashboard.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
+import TabInfoAnalysisNews from "../components/TabInfoAnalysisNews.jsx"
+
+
 
 const AuthorDashboard = () => {
+  const location = useLocation();
+
+  console.log(location);
+
   return (
-   <>
-   <div>cxfxdcgxcdgx</div>
-   </>
+    <>
+
+      <div className='bg-background pb-[7rem] mt-6'>
+        <TabInfoAnalysisNews author={location?.state?.author} />
+      </div>
+
+    </>
   )
 }
 
