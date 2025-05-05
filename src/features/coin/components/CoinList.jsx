5a694fd133f7c3c91f7046505aa1bf4b8812c038
Mohammad <mfelfelani72@gmail.com>
2025-05-05 08:29:03 +0000

    debug cash coin image in coin lists

--- src/features/coin/components/CoinList.jsx
+++ src/features/coin/components/CoinList.jsx
@@ -16,23 +16,23 @@ import { DEFAULT_COIN_IMAGE } from "../../../app/utils/constant/Defaults.js";
 const CoinList = ({ className, ...props }) => {
     // hooks
     const { t } = useTranslation();
+    console.log(props?.row?.logo);
 
     return (
         <>
             <div className='flex flex-row gap-2 items-center'>
                 <ImageLazy src={
-                    props?.cashed_images.length !== 0 &&
-                        props?.cashed_images?.some((item) =>
-                            item.hasOwnProperty(props?.row?.name)
-                        )
-                        ? props?.cashed_images.filter(
-                            (item) => item[props?.row?.name]
-                        )[0][props?.row?.name]?.base64data
-                        : props?.row?.logo
-                }
+                    props?.cashed_images?.length > 0 ?
+                        props.cashed_images.map((item) => {
+                            const imageData = item?.[props?.row?.name]?.base64data;
+                            return imageData ? imageData : null;
+                        }).filter(Boolean) : props?.row?.logo}
+                        
                     onError={(e) => {
                         e.target.src = DEFAULT_COIN_IMAGE;
-                    }} alt={props?.item?.name + "-logo"} className={"w-8 h-8 rounded-full"} />
+                    }}
+
+                    alt={props?.item?.name + "-logo"} className={"w-8 h-8 rounded-full"} />
                 <div className='flex flex-col w-full gap-2 justify-center'>
                     <div className="text-white text-sm font-medium  leading-none tracking-wide">{props?.row?.name}</div>
 
