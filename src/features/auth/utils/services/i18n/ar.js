a599d2fa71e66efb71119af7e5be3a47f7a9d64c
Mohammad <mfelfelani72@gmail.com>
2025-05-13 08:38:42 +0000

    build

--- src/features/auth/utils/services/i18n/ar.js
+++ src/features/auth/utils/services/i18n/ar.js
@@ -1,4 +1,16 @@
 const ar = {
+    // login
+    input_info_login: "الرجاء إدخال عنوان البريد الإلكتروني",
+    error_invalid_email: "البريد الإلكتروني غير صالح",
+    enter_password: "الرجاء إدخال كلمة المرور",
+    password: "كلمة المرور",
+    login: "تسجيل الدخول",
+    login_failed: "فشل تسجيل الدخول",
+    error_min_length_password: "يجب أن تتكون من 5 أحرف على الأقل",
 
+    // register
+    re_password: "تكرار كلمة المرور",
+    error_re_password: "كلمة المرور غير مطابقة لتكرار كلمة المرور",
+    invalid_email: "البريد الإلكتروني غير صالح",
 }
 export default ar;
